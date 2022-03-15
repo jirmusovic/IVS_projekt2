ffa668796a09d6aaa9b27c6e604efa69e5b78a4f
Explosiver001 <misel-olsanz@seznam.cz>
2022-03-15 22:08:50 +0100

    textbox test

--- src/Form1.cs
+++ src/Form1.cs
@@ -17,5 +17,9 @@ namespace Calc
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Working just fine";
+        }
     }
 }
