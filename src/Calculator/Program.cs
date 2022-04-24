aedeb98e46c52479f50025ea0012ef3aadc36d60
Explosiver001 <misel-olsany@seznam.cz>
2022-04-24 17:43:39 +0200

    Calc start upgrade

--- src/Calculator/Program.cs
+++ src/Calculator/Program.cs
@@ -23,40 +23,10 @@ namespace Calc
         [STAThread]
         static void Main()
         {
-            string res = String.Empty; /**Vysledek vyhodnoceni výbìrové smìrodatné odchylky*/
-#if false
-            string input = String.Empty; /**Promìnná pro uchování dat zadaných uživatelem*/
-            string line; /**Promìnná pro uchování aktuálnì pøeèteného øádku*/
-            
-            Task t = Task.Run(() =>  //Zaèátek operace, která èeká na uživatelský vstup
-            {
-                while ((line = Console.ReadLine()) != null) //Ètení všech øádkù ze vstupu
-                {
-                    if (input != String.Empty)
-                    {
-                        input += ' ';
-                    }
-                    input += line.Trim('\n');
-                }
-                if (input != String.Empty)
-                {
-                    Deviation dev = new Deviation();
-
-                    res = dev.StdDeviation(input);
-                    if (res != null) //Ovìøení správného výsledku operace
-                        Console.WriteLine(res);
-                    else
-                        Console.WriteLine("Chyba vstupu!");
-                }
-            });
-            TimeSpan ts = TimeSpan.FromMilliseconds(500);
-            if (!t.Wait(ts))
-                Console.WriteLine("Èas na zadání dat pro výbìrovou smìrodatnou odchylku vypršel!");
-#endif
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1(res)); //Spuštìní formuláøe
+            Application.Run(new Form1()); //Spuštìní formuláøe
         }
     }
 }
